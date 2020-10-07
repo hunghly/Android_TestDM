15a4fa754cf83e790b2dad455fa0d64fc36913c0
hunghly <hunghly@gmail.com>
2020-10-07 11:27:27 -0500

    tested layout

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.display.DisplayManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.content.Context;
 import android.view.Display;
 //import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private DisplayManager dManager;
+    private static Context context;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -39,14 +41,16 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        dManager = getSystemService((DisplayManager.class));
+//        context = getApplicationContext();
+        context = MainActivity.this;
+        dManager = context.getSystemService(DisplayManager.class);
         dManager.registerDisplayListener(mDisplayListener, null);
-        Display[] displays = dManager.getDisplays();
-        for (Display display : displays) {
+//        Display[] displays = dManager.getDisplays();
+//        for (Display display : displays) {
 //            System.out.println(display);
-            System.out.println("The Display is: " + display.getName());
-            System.out.println("The Display is: " + display.getDisplayId());
-        }
+//            System.out.println("The Display is: " + display.getName());
+//            System.out.println("The Display is: " + display.getDisplayId());
+//        }
         setContentView(R.layout.activity_main);
     }
 }
